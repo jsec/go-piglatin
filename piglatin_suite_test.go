0d1faad0a7350bbe2174ae11b4a18ebd27cced04
jsec <trollface@jsec.me>
2014-11-27 22:25:06 -0500

    add CI integration

--- piglatin_suite_test.go
+++ piglatin_suite_test.go
@@ -4,10 +4,12 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
+	"github.com/onsi/ginkgo/reporters"
 	"testing"
 )
 
 func TestPiglatin(t *testing.T) {
 	RegisterFailHandler(Fail)
-	RunSpecs(t, "Piglatin Suite")
+	junitReporter := reporters.NewJUnitReporter("junit.xml")
+	RunSpecsWithDefaultAndCustomReporters(t, "Piglatin Suite", []Reporter{junitReporter})
 }
